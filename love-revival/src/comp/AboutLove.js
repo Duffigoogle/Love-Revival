24a21f27a9926031da093a068a05743181c44af0
Caleb Duff <calebdduff@gmail.com>
2021-03-14 22:32:34 +0100

    added "about love" component.

--- love-revival/src/comp/AboutLove.js
+++ love-revival/src/comp/AboutLove.js
@@ -3,14 +3,25 @@ import '../styles/aboutLove.scss';
 
 function AboutLove() {
     return (
-        <div>
+        <div className='about-section'>
             <h1> About Love Revival </h1>
-            <section>
-                <div>
-                    <p> Lorem text </p>
+            <section className='about-content'>
+                <div className='about-text about'>
+                    <p> This arm focuses on spiritual awakening of the love of God in the
+                        lives of youths in our community (keeping oneself unspotted from
+                        the world). It is a night or evening of worship and a brief word of love.
+
+                        We partner with youth minisrty churches, having volunteers who
+                        serve either in the choir, technical, media / ICT, ushering, publicity,
+                        security, e.t.c to bring the event to pass. However, as the souls we
+                        invited encounter God in worship and give their life to Christ, we
+                        direct them to the churches we partner with for their spiritual
+                        growth while we enroll them into our Love Reform Program for their
+                        social change and integration back to society 
+                    </p>
                 </div>
-                <div>
-                    <img src='' alt=''></img>
+                <div className='about-img about'>
+                    <img src='love-revival\src\img\IMG_7063.png' alt='about-img'></img>
                 </div>
             </section>
         </div>
